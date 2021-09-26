a21d88ca58eb9ea068330fab926e9db9582f3411
Abu Rayhan <rayhan.cse15@gmail.com>
2021-09-26 15:49:08 +0600

    bootstrap 4.5.2 install

---
+++ client/src/Shared/Layout/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+
+export default function Dashboard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
