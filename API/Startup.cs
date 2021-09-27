6193a7d3bd287dfdd88518f7f8d4a78962996f87
Abu Rayhan <rayhan.cse15@gmail.com>
2021-09-27 18:23:09 +0600

    27-09-2021 update for Create Employee

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,6 +13,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Razor;
 
 namespace API
 {
@@ -34,6 +35,9 @@ namespace API
                     Configuration.GetConnectionString("DbConnection")));
 
             services.AddControllers();
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -47,6 +51,18 @@ namespace API
                                                 "http://www.contoso.com");
                         });
             });
+
+            #region Areas Config
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.AreaViewLocationFormats.Clear();
+                options.AreaViewLocationFormats.Add("/areas/{2}/Views/{1}/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/areas/{2}/Views/Shared/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+
+            });
+            #endregion
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,13 +78,27 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseStaticFiles();
             app.UseAuthorization();
             app.UseCors("CrosPolicy");
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "MyArea",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                    name: "controllers",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                //endpoints.MapControllerRoute(
+                //    name: "default",
+                //    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapRazorPages();
             });
+            
         }
     }
 }
--- client/src/Shared/Layout/Dashboard.tsx
+++ client/src/Shared/Layout/Dashboard.tsx
@@ -4,7 +4,8 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import { Col, Row } from 'react-bootstrap'
-import GetAllEmployee from '../../Models/Employee/GetAllEmployee'
+import GetAllEmployee from '../../Content/Employee/GetAllEmployee'
+import CreateEmployee from '../../Content/Employee/CreateEmployee'
 
 export default function Dashboard() {
     return (
@@ -13,7 +14,9 @@ export default function Dashboard() {
                 <Col md={8}>
                     <GetAllEmployee></GetAllEmployee>
                 </Col>
-                <Col md={4}></Col>
+                <Col md={4}>
+                    <CreateEmployee></CreateEmployee>
+                </Col>
             </Row>
         </div>
     )
